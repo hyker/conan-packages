1099a05de160b6c3028d965877d9dfb395126f2b
Joakim Brorsson <b.joakim@gmail.com>
2017-06-26 15:55:19 +0200

    [OpenSSL] Made fat lib for iOS (armv7, armv7s, arm64)

--- OpenSSL/1.0.2h/conanfile.py
+++ OpenSSL/1.0.2h/conanfile.py
@@ -43,160 +43,166 @@ class OpenSSLConan(ConanFile):
     source_tgz_sha256   = "1d4007e53aad94a5b2002fe045ee7bb0b3d98f1a47f8b2bc851dcd1c74332919"
     counter_config      = 0
 
-    def source(self):
-        self.output.info("Downloading %s" % self.source_tgz_filename)
-        try:
-            tools.download(self.source_tgz_old_url, self.source_tgz_filename)
-            tools.unzip(self.source_tgz_filename, ".")
-        except:
-            tools.download(self.source_tgz_url, self.source_tgz_filename)
-            tools.unzip(self.source_tgz_filename, ".")
+    # iOS
+    builder_ios_url     = "https://github.com/x2on/OpenSSL-for-iPhone.git"
+    builder_ios_folder  = "OpenSSL-for-iPhone"
 
-        tools.check_sha256(self.source_tgz_filename, self.source_tgz_sha256)
-        os.unlink(self.source_tgz_filename)
+    def source(self):
+        if self.settings.os == "iOS":
+            self.run("git clone %s" % self.builder_ios_url)
+        else:
+            self.output.info("Downloading %s" % self.source_tgz_filename)
+            try:
+                tools.download(self.source_tgz_old_url, self.source_tgz_filename)
+                tools.unzip(self.source_tgz_filename, ".")
+            except:
+                tools.download(self.source_tgz_url, self.source_tgz_filename)
+                tools.unzip(self.source_tgz_filename, ".")
+
+            tools.check_sha256(self.source_tgz_filename, self.source_tgz_sha256)
+            os.unlink(self.source_tgz_filename)
 
     def config(self):
-        self.counter_config += 1
-        del self.settings.compiler.libcxx
-
-        if not self.options.no_electric_fence and self.settings.os == "Linux":
-            private = False if self.options.shared else True
-            if self.counter_config==2:
-                self.requires.add("electric-fence/2.2.0@lasote/stable", private=private)
-            self.options["electric-fence"].shared = self.options.shared
-        else:
-            if "electric-fence" in self.requires:
-                del self.requires["electric-fence"]
-
-        if not self.options.no_zlib:
-            self.requires.add("zlib/1.2.8@lasote/stable", private=False)
-            self.options["zlib"].shared = self.options.zlib_dynamic
-
-        else:
-            if "zlib" in self.requires:
-                del self.requires["zlib"]
+        if not self.settings.os == "iOS":
+            self.counter_config += 1
+            del self.settings.compiler.libcxx
+    
+            if not self.options.no_electric_fence and self.settings.os == "Linux":
+                private = False if self.options.shared else True
+                if self.counter_config==2:
+                    self.requires.add("electric-fence/2.2.0@lasote/stable", private=private)
+                self.options["electric-fence"].shared = self.options.shared
+            else:
+                if "electric-fence" in self.requires:
+                    del self.requires["electric-fence"]
+    
+            if not self.options.no_zlib:
+                self.requires.add("zlib/1.2.8@lasote/stable", private=False)
+                self.options["zlib"].shared = self.options.zlib_dynamic
+    
+            else:
+                if "zlib" in self.requires:
+                    del self.requires["zlib"]
 
     @property
     def subfolder(self):
-        return "openssl-%s" % self.version
+        if self.settings.os == "iOS":
+            return self.builder_ios_folder
+        else:
+            return "openssl-%s" % self.version
 
     def build(self):
-        config_options_string = ""
-
-        if self.deps_cpp_info.include_paths:
-            if "zlib" in self.requires:
-                include_path = self.deps_cpp_info["zlib"].include_paths[0]
-                if self.settings.os == "Windows":
-                    lib_path = self.deps_cpp_info["zlib"].lib_paths[0] + "/" + self.deps_cpp_info["zlib"].libs[0] + ".lib"
+        if self.settings.os == "iOS":
+            self.run("cd %s && ./build-libssl.sh --version=%s" % (self.builder_ios_folder, self.version))
+        else:
+            config_options_string = ""
+    
+            if self.deps_cpp_info.include_paths:
+                if "zlib" in self.requires:
+                    include_path = self.deps_cpp_info["zlib"].include_paths[0]
+                    if self.settings.os == "Windows":
+                        lib_path = self.deps_cpp_info["zlib"].lib_paths[0] + "/" + self.deps_cpp_info["zlib"].libs[0] + ".lib"
+                    else:
+                        lib_path = self.deps_cpp_info["zlib"].lib_paths[0]
+                    config_options_string += ' --with-zlib-include="%s"' % include_path
+                    config_options_string += ' --with-zlib-lib="%s"' % lib_path
+    
+                if "electric-fence" in self.requires:
+                    libs = " ".join([ "-l%s" % lib for lib in self.deps_cpp_info["electric-fence"].libs])
+                    config_options_string += ' -L"%s" -I"%s" %s' % (self.deps_cpp_info["electric-fence"].lib_paths[0],
+                                                                    self.deps_cpp_info["electric-fence"].include_paths[0],
+                                                                    libs)
                 else:
-                    lib_path = self.deps_cpp_info["zlib"].lib_paths[0]
-                config_options_string += ' --with-zlib-include="%s"' % include_path
-                config_options_string += ' --with-zlib-lib="%s"' % lib_path
-
-            if "electric-fence" in self.requires:
-                libs = " ".join([ "-l%s" % lib for lib in self.deps_cpp_info["electric-fence"].libs])
-                config_options_string += ' -L"%s" -I"%s" %s' % (self.deps_cpp_info["electric-fence"].lib_paths[0],
-                                                                self.deps_cpp_info["electric-fence"].include_paths[0],
-                                                                libs)
-            else:
-                replace_in_file("./openssl-%s/Configure" % self.version, "::-lefence::", "::")
-                replace_in_file("./openssl-%s/Configure" % self.version, "::-lefence ", "::")
-            
-            self.output.warn("=====> Options: %s" % config_options_string)
-
-        for option_name in self.options.values.fields:
-            activated = getattr(self.options, option_name)
-            if activated:
-                self.output.info("Activated option! %s" % option_name)
-                config_options_string += " %s" % option_name.replace("_", "-")
-
-        def run_in_src(command, show_output=False):
-            command = 'cd openssl-%s && %s' % (self.version, command)
-            if not show_output and self.settings.os != "Windows":
-                command += ' | while read line; do echo -n "."; done'
-            self.run(command)
-            self.output.writeln(" ")
-
-        def unix_make(config_options_string):
-            self.output.warn("----------CONFIGURING OPENSSL %s-------------" % self.version)
-            m32_suff = " -m32" if self.settings.arch == "x86" else ""
-            if self.settings.os == "Linux":
-                if self.settings.build_type == "Debug":
-                    config_options_string = "-d " + config_options_string
-
-                m32_pref = "setarch i386" if self.settings.arch == "x86" else ""
-                config_line = "%s ./config -fPIC %s %s" % (m32_pref, config_options_string, m32_suff)
-                self.output.warn(config_line)
-                run_in_src(config_line)
-                run_in_src("make depend")
-                self.output.warn("----------MAKE OPENSSL %s-------------" % self.version)
-                run_in_src("make")
-            elif self.settings.os == "Macos":
-                if self.settings.arch == "x86_64":
-                    command = "./Configure darwin64-x86_64-cc %s" % config_options_string
+                    replace_in_file("./openssl-%s/Configure" % self.version, "::-lefence::", "::")
+                    replace_in_file("./openssl-%s/Configure" % self.version, "::-lefence ", "::")
+                
+                self.output.warn("=====> Options: %s" % config_options_string)
+    
+            for option_name in self.options.values.fields:
+                activated = getattr(self.options, option_name)
+                if activated:
+                    self.output.info("Activated option! %s" % option_name)
+                    config_options_string += " %s" % option_name.replace("_", "-")
+    
+            def run_in_src(command, show_output=False):
+                command = 'cd openssl-%s && %s' % (self.version, command)
+                if not show_output and self.settings.os != "Windows":
+                    command += ' | while read line; do echo -n "."; done'
+                self.run(command)
+                self.output.writeln(" ")
+    
+            def unix_make(config_options_string):
+                self.output.warn("----------CONFIGURING OPENSSL %s-------------" % self.version)
+                m32_suff = " -m32" if self.settings.arch == "x86" else ""
+                if self.settings.os == "Linux":
+                    if self.settings.build_type == "Debug":
+                        config_options_string = "-d " + config_options_string
+    
+                    m32_pref = "setarch i386" if self.settings.arch == "x86" else ""
+                    config_line = "%s ./config -fPIC %s %s" % (m32_pref, config_options_string, m32_suff)
+                    self.output.warn(config_line)
+                    run_in_src(config_line)
+                    run_in_src("make depend")
+                    self.output.warn("----------MAKE OPENSSL %s-------------" % self.version)
+                    run_in_src("make")
+                elif self.settings.os == "Macos":
+                    if self.settings.arch == "x86_64":
+                        command = "./Configure darwin64-x86_64-cc %s" % config_options_string
+                    else:
+                        command = "./config %s %s" % (config_options_string, m32_suff)
+                    run_in_src(command)
+                    # REPLACE -install_name FOR FOLLOW THE CONAN RULES,
+                    # DYNLIBS IDS AND OTHER DYNLIB DEPS WITHOUT PATH, JUST THE LIBRARY NAME
+                    old_str = 'SHAREDFLAGS="$$SHAREDFLAGS -install_name $(INSTALLTOP)/$(LIBDIR)/$$SHLIB$'
+                    new_str = 'SHAREDFLAGS="$$SHAREDFLAGS -install_name $$SHLIB$'
+                    replace_in_file("./openssl-%s/Makefile.shared" % self.version, old_str, new_str)
+                    self.output.warn("----------MAKE OPENSSL %s-------------" % self.version)
+                    run_in_src("make")
+    
+            def windows_make(config_options_string):
+                self.output.warn("----------CONFIGURING OPENSSL FOR WINDOWS. %s-------------" % self.version)
+                debug = "debug-" if self.settings.build_type == "Debug" else ""
+                arch = "32" if self.settings.arch == "x86" else "64A"
+                configure_type = debug + "VC-WIN" + arch
+                # Will output binaries to ./binaries
+                config_command = "perl Configure %s no-asm --prefix=../binaries" % configure_type
+                whole_command = "%s %s" % (config_command, config_options_string)
+                self.output.warn(whole_command)
+                run_in_src(whole_command)
+    
+                if self.options.no_asm:
+                    run_in_src("ms\do_nasm")
+    
+                if arch == "64A":
+                    run_in_src("ms\do_win64a")
                 else:
-                    command = "./config %s %s" % (config_options_string, m32_suff)
-                run_in_src(command)
-                # REPLACE -install_name FOR FOLLOW THE CONAN RULES,
-                # DYNLIBS IDS AND OTHER DYNLIB DEPS WITHOUT PATH, JUST THE LIBRARY NAME
-                old_str = 'SHAREDFLAGS="$$SHAREDFLAGS -install_name $(INSTALLTOP)/$(LIBDIR)/$$SHLIB$'
-                new_str = 'SHAREDFLAGS="$$SHAREDFLAGS -install_name $$SHLIB$'
-                replace_in_file("./openssl-%s/Makefile.shared" % self.version, old_str, new_str)
+                    run_in_src("ms\do_ms")
+                runtime = self.settings.compiler.runtime
+                # Replace runtime in ntdll.mak and nt.mak
+                replace_in_file("./openssl-%s/ms/ntdll.mak" % self.version, "/MD ", "/%s " % runtime)
+                replace_in_file("./openssl-%s/ms/nt.mak" % self.version, "/MT ", "/%s " % runtime)
+                replace_in_file("./openssl-%s/ms/ntdll.mak" % self.version, "/MDd ", "/%s " % runtime)
+                replace_in_file("./openssl-%s/ms/nt.mak" % self.version, "/MTd ", "/%s " % runtime)
+    
+                self.output.warn(os.curdir)
+                make_command = "nmake -f ms\\ntdll.mak" if self.options.shared else "nmake -f ms\\nt.mak "
                 self.output.warn("----------MAKE OPENSSL %s-------------" % self.version)
-                run_in_src("make")
-            elif self.settings.os == "iOS":
-                run_in_src("./Configure iphoneos-cross shared no-dso no-hw no-engine %s" % config_options_string)
-                old_str = 'CFLAG= -fPIC'
-                new_str = 'CFLAG= -arch %s -fPIC' % self.settings.arch
-                replace_in_file("./openssl-%s/Makefile" % self.version, old_str, new_str)
-                self.output.warn("----------MAKE OPENSSL %s-------------" % self.version)
-                run_in_src("make depend")
-                run_in_src("make")
-
-        def windows_make(config_options_string):
-            self.output.warn("----------CONFIGURING OPENSSL FOR WINDOWS. %s-------------" % self.version)
-            debug = "debug-" if self.settings.build_type == "Debug" else ""
-            arch = "32" if self.settings.arch == "x86" else "64A"
-            configure_type = debug + "VC-WIN" + arch
-            # Will output binaries to ./binaries
-            config_command = "perl Configure %s no-asm --prefix=../binaries" % configure_type
-            whole_command = "%s %s" % (config_command, config_options_string)
-            self.output.warn(whole_command)
-            run_in_src(whole_command)
-
-            if self.options.no_asm:
-                run_in_src("ms\do_nasm")
-
-            if arch == "64A":
-                run_in_src("ms\do_win64a")
-            else:
-                run_in_src("ms\do_ms")
-            runtime = self.settings.compiler.runtime
-            # Replace runtime in ntdll.mak and nt.mak
-            replace_in_file("./openssl-%s/ms/ntdll.mak" % self.version, "/MD ", "/%s " % runtime)
-            replace_in_file("./openssl-%s/ms/nt.mak" % self.version, "/MT ", "/%s " % runtime)
-            replace_in_file("./openssl-%s/ms/ntdll.mak" % self.version, "/MDd ", "/%s " % runtime)
-            replace_in_file("./openssl-%s/ms/nt.mak" % self.version, "/MTd ", "/%s " % runtime)
-
-            self.output.warn(os.curdir)
-            make_command = "nmake -f ms\\ntdll.mak" if self.options.shared else "nmake -f ms\\nt.mak "
-            self.output.warn("----------MAKE OPENSSL %s-------------" % self.version)
-            run_in_src(make_command)
-            run_in_src("%s install" % make_command)
-            # Rename libs with the arch
-            renames = {"./binaries/lib/libeay32.lib": "./binaries/lib/libeay32%s.lib" % runtime,
-                       "./binaries/lib/ssleay32.lib": "./binaries/lib/ssleay32%s.lib" % runtime}
-            for old, new in renames.iteritems():
-                if os.path.exists(old):
-                    os.rename(old, new)
-
-        if self.settings.os == "Linux" or self.settings.os == "Macos" or self.settings.os == "iOS":
-            unix_make(config_options_string)
-        elif self.settings.os == "Windows":
-            windows_make(config_options_string)
-
-        self.output.info("----------BUILD END-------------")
-        return
+                run_in_src(make_command)
+                run_in_src("%s install" % make_command)
+                # Rename libs with the arch
+                renames = {"./binaries/lib/libeay32.lib": "./binaries/lib/libeay32%s.lib" % runtime,
+                           "./binaries/lib/ssleay32.lib": "./binaries/lib/ssleay32%s.lib" % runtime}
+                for old, new in renames.iteritems():
+                    if os.path.exists(old):
+                        os.rename(old, new)
+    
+            if self.settings.os == "Linux" or self.settings.os == "Macos" or self.settings.os == "iOS":
+                unix_make(config_options_string)
+            elif self.settings.os == "Windows":
+                windows_make(config_options_string)
+    
+            self.output.info("----------BUILD END-------------")
+            return
 
     def package(self):
         self.copy("FindOpenSSL.cmake", ".", ".")
