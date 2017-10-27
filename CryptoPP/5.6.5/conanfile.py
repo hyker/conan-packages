b19357533ea2dfb587ffa00803ae8c089f5dcebe
Michał Mąka <michal.maka@voicepin.com>
2017-10-27 14:20:28 +0200

    Added some CMake options provided by CryptoPP and changed hand crafted execution of CMake to using of CMake build helper

--- CryptoPP/5.6.5/conanfile.py
+++ CryptoPP/5.6.5/conanfile.py
@@ -9,9 +9,21 @@ class CryptoppConan(ConanFile):
     url               = "https://github.com/hykersec/conan-packages"
     settings          = "os", "compiler", "build_type", "arch"
     options           = {
-        "shared": [True, False]
+        "shared": [False, True],
+        "disable_asm": [False, True], # This and next options are described in https://www.cryptopp.com/wiki/CMake
+        "disable_sse3": [False, True],
+        "disable_sse4": [False, True],
+        "disable_aesni": [False, True],
+        "disable_sha": [False, True],
+        "native_arch": [False, True],
     }
-    default_options   = "=False\n".join(options.keys()) + "=False"
+    default_options   = "shared=False", \
+        "disable_asm=False", \
+        "disable_sse3=False", \
+        "disable_sse4=False", \
+        "disable_aesni=False", \
+        "disable_sha=False", \
+        "native_arch=False"
     generators        = "cmake"
     source_git_url    = "https://github.com/weidai11/cryptopp.git"
     source_git_commit = "aaf62695fc03bf941ec51e40a139f5e0eb8652f3"
@@ -36,9 +48,22 @@ conan_basic_setup()''')
 
             self.run("cd cryptopp && lipo -create %s -output ./libcryptopp.a" % (" ".join(["./libcryptopp-%s.a" % arch for arch in arches])))
         else:
-            cmake = CMake(self)
-            self.run('cmake cryptopp %s %s' % (cmake.command_line, "-DBUILD_SHARED_LIBS=ON" if self.options.shared else ""))
-            self.run("cmake --build . %s" % cmake.build_config)
+            cmake = CMake(self, parallel=True)
+            
+            on_off_map = {False: "OFF", True: "ON"}            
+            cmake.definitions["DISABLE_ASM"] = on_off_map[bool(self.options.disable_asm)]
+            cmake.definitions["DISABLE_SSSE3"] = on_off_map[bool(self.options.disable_sse3)]
+            cmake.definitions["DISABLE_SSE4"] = on_off_map[bool(self.options.disable_sse4)]
+            cmake.definitions["DISABLE_AESNI"] = on_off_map[bool(self.options.disable_aesni)]
+            cmake.definitions["DISABLE_SHA"] = on_off_map[bool(self.options.disable_sha)]
+            cmake.definitions["CRYPTOPP_NATIVE_ARCH"] = on_off_map[bool(self.options.native_arch)]
+            
+            cmake.definitions["BUILD_SHARED"] = on_off_map[bool(self.options.shared)]
+            cmake.definitions["BUILD_STATIC"] = on_off_map[bool(not self.options.shared)]
+            
+            self.output.info("Confgigured with %s" % cmake.command_line)
+            cmake.configure(source_dir="cryptopp")
+            cmake.build()
 
     def package(self):
         self.copy("*.h", dst="include/cryptopp", src="cryptopp")
